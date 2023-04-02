787eaf509e735b3513f858dac55dbbe6022b613f
Enchong <deevera.enchong18@gmail.com>
2023-04-02 15:03:24 +0800

    admin and user dashboard

--- resources/views/partials/admin/left-sidebar.blade.php
+++ resources/views/partials/admin/left-sidebar.blade.php
@@ -38,34 +38,35 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-
-           <li class="nav-item">
-            {{-- <a href="">
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
-              </p>
-            </a> --}}
-          </li>
-          <li class="nav-item {{$current_route=='users.index'?'menu-open':''}}">
-            <a href="#" class="nav-link {{$current_route=='users.index'?'active':''}}">
-              <i class="nav-icon fas fa-users"></i>
-              <p>
-                User Management
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="">
-                  <i class="far fas fa-user"></i>
-                  <p>Users</p>
+                <a href="./index.html" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Dashboard v1</p>
                 </a>
               </li>
-
+              <li class="nav-item">
+                <a href="./index2.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Dashboard v2</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./index3.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Dashboard v3</p>
+                </a>
+              </li>
             </ul>
           </li>
-
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
