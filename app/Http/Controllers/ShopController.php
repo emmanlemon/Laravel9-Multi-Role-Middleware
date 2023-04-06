736e1135d0bdf0b0669648ba7eba1a8a9e8ad76f
deeveraEnchongOfficial <deveraflorencio18@gmail.com>
2023-04-06 23:46:44 +0800

    updated

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -57,12 +57,13 @@ class ShopController extends Controller
      */
     public function show($id)
     {
-        // $usersAndShops = DB::table('users')
-        // ->join('shops', 'users.id', '=', 'shops.users_id')
-        // ->select('users.*', 'shops.*')
-        // ->where('shops.id', '=', $id)
-        // ->get();
+        $usersAndShops = DB::table('users')
+        ->join('shops', 'users.id', '=', 'shops.users_id')
+        ->select('users.*', 'shops.*')
+        ->where('shops.id', '=', $id)
+        ->get();
 
+        return view('superadmin.shop.view', compact('usersAndShops'));
     }
 
     /**
