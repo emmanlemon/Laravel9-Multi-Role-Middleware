c955c78f866030f267e3e961cc892af152d190e7
deeveraEnchongOfficial <deveraflorencio18@gmail.com>
2023-04-09 21:20:54 +0800

    wip user service

--- resources/views/user/shop/view.blade.php
+++ resources/views/user/shop/view.blade.php
@@ -40,83 +40,116 @@
                     </div>
 
 
-    
 
-                    <!-- Blade Template -->
-                    <div class="form-group">
-                        <label for="vehicle_list">Vehicle List</label>
-                        <select name="vehicle_list" id="vehicle_list" class="form-control">
-                          <option value="">-- Select Vehicle --</option>
-                          @foreach($vehicle_lists as $vehicle)
-                            <option value="{{ $vehicle->id }}">{{ $vehicle->vehicle_type }}</option>
-                          @endforeach
-                        </select>
-                      </div>
-                      
-                      <div class="form-group">
-                        <label for="service_list">Service List</label>
-                        <div class="table-responsive">
-                          <table id="service_table" class="table">
-                            <thead>
-                              <tr>
-                                <th></th>
-                                <th>Service Name</th>
-                                <th>Service Price</th>
-                              </tr>
-                            </thead>
-                            <tbody></tbody>
-                          </table>
+
+
+
+
+
+                    <form id="service_form" method="POST" action="">
+                        @csrf
+                        
+                        <div class="form-group">
+                            <label for="vehicle_list">Vehicle List</label>
+                            <select name="vehicle_list" id="vehicle_list" class="form-control">
+                                <option value="">-- Select Vehicle --</option>
+                                @foreach($vehicle_lists as $vehicle)
+                                    <option value="{{ $vehicle->id }}">{{ $vehicle->vehicle_type }}</option>
+                                @endforeach
+                            </select>
                         </div>
-                        <div class="row">
-                          <div class="col-sm-6">
-                            <strong>Total Price: </strong><span id="total_price">$0.00</span>
-                          </div>
+                        
+                        <div class="form-group">
+                            <label for="service_list">Service List</label>
+                            <table id="service_table">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Service Name</th>
+                                        <th>Service Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
                         </div>
-                      </div>
-                      
-                      <style>
+                        
+                        <div class="form-group">
+                            <label for="total_price">Total Price:</label>
+                            <input type="text" name="total_price" id="total_price" class="form-control" readonly>
+                        </div>
+                        
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+                    <style>
+                        #service_table {
+                            border-collapse: collapse;
+                            width: 100%;
+                        }
+                    
                         #service_table td,
                         #service_table th {
-                          border: 1px solid #ddd;
-                          padding: 8px;
-                          text-align: left;
+                            border: 1px solid #ddd;
+                            padding: 8px;
+                            text-align: left;
                         }
-                      
+                    
                         #service_table th {
-                          background-color: #f2f2f2;
+                            background-color: #f2f2f2;
                         }
-                      </style>
-                      
-                      <script>
+                    </style>
+                    
+                    <script>
                         $(document).ready(function() {
-                          $('#vehicle_list').change(function() {
-                            var vehicle_lists_id = $(this).val();
-                      
-                            $.ajax({
-                              url: "{{ route('getServices') }}",
-                              type: "GET",
-                              data: {vehicle_lists_id: vehicle_lists_id},
-                              dataType: "json",
-                              success: function(data) {
-                                var options = '';
-                                $.each(data, function(key, value) {
-                                  options += '<tr><td><input type="checkbox" name="services[]" value="' + value.id + '"></td><td>' + value.service + '</td><td>$' + value.price + '</td></tr>';
+                            $('#vehicle_list').change(function() {
+                                var vehicle_lists_id = $(this).val();
+                        
+                                $.ajax({
+                                    url: "{{ route('getServices') }}",
+                                    type: "GET",
+                                    data: {vehicle_lists_id: vehicle_lists_id},
+                                    dataType: "json",
+                                    success: function(data) {
+                                        var options = '';
+                                        $.each(data, function(key, value) {
+                                            options += '<tr><td><input type="checkbox" name="services[]" value="' + value.id + '" data-price="' + value.price + '"></td><td>' + value.service + '</td><td>$' + value.price + '</td></tr>';
+                                        });
+                                        $('#service_table tbody').html(options);
+                                    }
                                 });
-                                $('#service_table tbody').html(options);
-                              }
                             });
-                          });
-                      
-                          $('#service_table').on('change', 'input[type="checkbox"][name="services[]"]', function() {
-                            var total = 0;
-                            $('input[type="checkbox"][name="services[]"]:checked').each(function() {
-                              var price = parseFloat($(this).closest('tr').find('td:nth-child(3)').text().replace('$', ''));
-                              total += price;
+                            
+                            $('#service_table').on('change', 'input[type="checkbox"]', function() {
+                                var total = 0;
+                                
+                                $('input[name="services[]"]:checked').each(function() {
+                                    total += parseFloat($(this).data('price'));
+                                });
+                                
+                                $('#total_price').val(total.toFixed(2));
                             });
-                            $('#total_price').text('$' + total.toFixed(2));
-                          });
+                            
+                            $('#service_form').submit(function(e) {
+                                e.preventDefault();
+                                
+                                var form = $(this);
+                                
+                                $.ajax({
+                                    url: form.attr('action'),
+                                    type: form.attr('method'),
+                                    data: form.serialize(),
+                                    success: function(response) {
+                                        // handle success response
+                                    },
+                                    error: function(xhr, status, error) {
+                                        // handle error response
+                                    }
+                                });
+                            });
                         });
-                      </script>
+                    </script>
+
+
                     
                     
                 </div>
